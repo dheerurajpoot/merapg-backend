57c3ef20e378b009048fd9124483d67dc2e28224
Dheeru Rajpoot <150139748+dheerurajpoot@users.noreply.github.com>
2024-08-16 10:43:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 });
 app.use(
 	cors({
-		origin: `${process.env.FRONTEND_URL}`,
+		origin: [`${process.env.FRONTEND_URL}`, "http://localhost:5173"],
 		methods: "GET, POST, PATCH, DELETE, PUT",
 		credentials: true,
 	})
