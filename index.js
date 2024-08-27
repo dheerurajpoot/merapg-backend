89cce0e5f5dfa00f65de63bf432bfb2c18be8ae9
Dheeru Rajpoot <150139748+dheerurajpoot@users.noreply.github.com>
2024-08-27 13:50:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,21 +18,6 @@ const app = express();
 
 dbConnect();
 
-app.use((req, res, next) => {
-	console.log("Origin: ", req.headers.origin);
-	console.log("Host: ", req.headers.host);
-	next();
-});
-
-// app.use((req, res, next) => {
-// 	res.header("Access-Control-Allow-Origin", "https://www.merapg.com");
-// 	res.header(
-// 		"Access-Control-Allow-Headers",
-// 		"Origin, X-Requested-With, Content-Type, Accept"
-// 	);
-// 	next();
-// });
-
 app.use(
 	cors({
 		origin: "https://www.merapg.com",
