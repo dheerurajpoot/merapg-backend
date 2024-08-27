efda3c674d4ac181c8e4e8d1a23481bb744f7cb2
Dheeru Rajpoot <150139748+dheerurajpoot@users.noreply.github.com>
2024-08-27 09:38:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,15 @@ const app = express();
 
 dbConnect();
 
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
+	next();
+});
+
 // app.use(
 // 	cors({
 // 		origin: [
