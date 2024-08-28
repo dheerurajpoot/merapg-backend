2645276596d26c4d712fb2cc86179c919da9456c
Dheeru Rajpoot <150139748+dheerurajpoot@users.noreply.github.com>
2024-08-28 07:31:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,11 @@ dbConnect();
 
 app.use(
 	cors({
-		origin: "https://www.merapg.com",
+		origin: [
+			"https://www.merapg.com",
+			"http://localhost:5173",
+			"https://merapg.com",
+		],
 		methods: "GET, POST, PATCH, DELETE, PUT",
 		credentials: true,
 		allowedHeaders: ["Content-Type", "Authorization", "X-Custom-Header"],
