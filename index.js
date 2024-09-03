0235cdefc0aaa2861dec6ab769a183290fa1d914
Dheeru Rajpoot <150139748+dheerurajpoot@users.noreply.github.com>
2024-09-03 11:54:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ app.use(
 );
 app.use(morgan("dev"));
 app.use(bodyParser.json());
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(express.json({ limit: "40mb" }));
+app.use(express.urlencoded({ extended: true, limit: "40mb" }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
